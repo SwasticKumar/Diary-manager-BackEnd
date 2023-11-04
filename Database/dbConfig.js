8db25e00d2a9703554929d8be83a19cfbe8446e7
SwasticKumar <kswastic@gmail.com>
2023-11-05 02:11:11 +0530

    inital commit

---
+++ Database/dbConfig.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const BASE_DB=process.env.DB_URI
+
+// const BASE_URL = process.env.NODE_ENVIRONMENT === 'development'
+//                     ?   `mongodb://0.0.0.0:27017/${process.env.DEVELOPMENT_MONGO_DB_NAME}`
+//                     :  `mongodb+srv://${process.env.PRODUCTION_MONGO_USER_NAME}:${process.env.PRODUCTION_MONGO_PASSWORD}@cluster0.oygceyq.mongodb.net/${process.env.PRODUCTION_MONGO_DB_NAME}` ;
+
+
+mongoose.connect(BASE_DB)
+.then(response => {
+    console.log("DATABASE connection successfully");
+})
+.catch(err => {
+    console.log("DATABASE connection failed");
+})
